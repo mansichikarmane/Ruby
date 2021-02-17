d6bebeb9f22f5a307b71b6248b21d8c759851cc2
Mansi <mansi.chikarmane@gmail.com>
2021-02-17 14:32:48 -0800

    edit import file

---
+++ W5D3/import_db.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS users
+DROP TABLE IF EXISTS questions
+DROP TABLE IF EXISTS question_follows
+DROP TABLE IF EXISTS replies
+DROP TABLE IF EXISTS question_likes
+
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+);
+
+CREATE TABLE questions(
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+  FOREIGN KEY(author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows(
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies(
+  id INTEGER PRIMARY KEY,
+  author_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  parent_reply_id INTEGER,
+  body TEXT NOT NULL,
+  FOREIGN KEY(author_id) REFERENCES users(id),
+  FOREIGN KEY(question_id) REFERENCES questions(id),
+  FOREIGN KEY(parent_reply_id) REFERENCES replies(id)
+);
+
+CREATE TABLE question_likes(
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(question_id) REFERENCES questions(id
+);
+
+INSERT INTO 
+  question_likes(id)
+VALUES
