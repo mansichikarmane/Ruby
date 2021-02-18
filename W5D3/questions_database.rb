6dd7f04515cf1211f998181875b6d6ae408fcd7a
Mansi <mansi.chikarmane@gmail.com>
2021-02-17 17:00:27 -0800

    finish ruby

--- W5D3/questions_database.rb
+++ W5D3/questions_database.rb
@@ -1,6 +1,68 @@
-class QuestionsDatabase
+require 'sqlite3'
+require 'singleton'
 
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
 
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
 
+class Question
+  attr_accessor :id, :title, :body, :author_id
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+    data.map { |datum| Question.new(datum) }
+  end
 
+  def self.find_by_id(id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = ?
+    SQL
+    return nil unless question.length > 0
+    Question.new(question.first) # question stored in array
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+end
+
+class User
+  attr_accessor :fname, :lname, :id
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+    data.map { |datum| User.new(datum) }
+  end
+
+  def self.find_by_name(fname, lname)
+    user = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+    SELECT
+        *
+      FROM
+        users
+      WHERE
+        fname = ?
+        lname = ?
+    SQL
+    return nil unless user.length > 0 
+    User.new(user.first)
+  end 
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
 end
