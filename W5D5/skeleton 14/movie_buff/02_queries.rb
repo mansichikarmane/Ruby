503d8a7e45f49ede5210b61a57a21ef3875fbc9e
Mansi <mansi.chikarmane@gmail.com>
2021-02-19 15:10:13 -0800

    queries 3

--- W5D5/skeleton 14/movie_buff/02_queries.rb
+++ W5D5/skeleton 14/movie_buff/02_queries.rb
@@ -20,8 +20,10 @@ def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
   Actor
-    .select(:name)
-    .joins(:)
+    .select(:id, :name)
+    .joins(:movies)
+    .where(movies: {title: title})
+    .order('castings.ord')
 end
 
 def vanity_projects
@@ -30,11 +32,21 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
-
+  Movie 
+    .select('movies.id, movies.title, actors.name')
+    .joins(:actors)
+    .where('director_id = actors.id')
+    .where('castings.ord = 1')
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
-
+  Actor
+    .select(:id, :name, 'COUNT(castings.actor_id) AS roles')
+    .joins(:castings)
+    .where.not(castings: {ord: 1})
+    .group('actors.id')
+    .order('roles DESC')
+    .limit(2)
 end
