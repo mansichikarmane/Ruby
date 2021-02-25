a4b1a2484d08cd581f8f16d0ae4126fe4cf24594
mayagbarnes <maya8617@gmail.com>
2021-02-24 16:26:48 -0800

    Finish routes and controllers project

--- W6D3/Project/app/controllers/users_controller.rb
+++ W6D3/Project/app/controllers/users_controller.rb
@@ -1,14 +1,42 @@
 class UsersController < ApplicationController
 
   def index
-    render json: params
+    users = User.all
+    render json: users
   end
 
   def create
-    render json: params
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
-    render json: params
+    user = User.find_by(id: params[:id])
+    render json: user
+  end
+
+  def update
+      user = User.find_by(id: params[:id])
+      if user.update(user_params)
+        render json: user
+      else
+        render json: user.errors.full_messages, status: :unprocessable_entity
+      end
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: user
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:name, :email)
   end
 end
--- W6D3/Project/config/routes.rb
+++ W6D3/Project/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post 'users', to: 'users#create'
   patch 'users/:id', to: 'users#update'
   put 'users/:id', to: 'users#update'
-  delete 'users/id', to: 'users#destroy'
+  delete 'users/:id', to: 'users#destroy'
   get 'users/new', to:'users#new', as: 'new_user'
   get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
 
