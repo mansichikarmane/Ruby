c8c57a5a60706256c90f2ecd8aec807fb06685f3
Mansi <mansi.chikarmane@gmail.com>
2021-02-26 14:05:39 -0800

    made cat model

---
+++ W6D5/99 cats/cats/app/models/cat.rb
@@ -0,0 +1,27 @@
+require "action_view"
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string           not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+  COLORS = ['orange', 'black', 'white', 'gray']
+  validates :birth_date, :color, :name, :sex, presence: true
+  validates :color, inclusion: {in: COLORS}
+
+  
+
+
+  def age
+    Date.now - self.birth_date
+  end
+end
