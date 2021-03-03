7b7d777e94fdb31e22ad2500f4776571b6441ee8
Mansi <mansi.chikarmane@gmail.com>
2021-03-03 14:33:01 -0800

    wrote specs

---
+++ W7D3/goals_app/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  let(:valid_params) {{user: {username: 'lilly_llama', password: 'password'}}}
+  let(:invalid_params) {{user: {username: 'bob', password: ''}}}
+  let(:user) { User.find_by(username: 'lilly_llama')}
+  describe "POST #create" do
+    context "with valid params" do
+      before :each do
+        post :create, params: valid_params
+      end
+      it "creates user, logs them in and redirects to the user's show page" do
+        expect(User.last.username).to eq("lilly_llama")
+        expect(session[:session_token]).to eq(user.session_token)
+        expect(response).to redirect_to(user_url(user))
+      end
+
+    end
+
+    context "with invalid params" do
+      before :each do
+        post :create, params: invalid_params
+      end
+        it "renders the new template" do
+          expect(response).to render_template(:new)
+        end
+        # it "adds errors to flash" do
+        #   expect(flash[:errors]).to be_present
+        # end
+    end 
+  end
+
+  describe " GET #new" do
+    it "renders the new user template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  
+end
