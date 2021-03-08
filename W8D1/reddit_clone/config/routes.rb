5d0e11fe78015aa58d6562672b95bced5e2177ed
tpadgett <tim_padgett@yahoo.com>
2021-03-08 10:20:49 -0800

    building out subs

--- W8D1/reddit_clone/config/routes.rb
+++ W8D1/reddit_clone/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:new, :create, :show, :index]
+  resource :session, only: [:new, :create, :destroy]
+
+  resources :subs, only: [:new, :create, :show, :index, :update, :edit]
+
 end
